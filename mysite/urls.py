2bfe210bc8304d7244be6c2faa015833654fd5a6
Oliver022 <alexlee022@gmail.com>
2014-04-12 01:39:13 -0700

    show index

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     url(r'^$', 'bestrecipe.views.index'),
                        # url(r'^blog/', include('blog.urls')),
     (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
-    url(r'^admin/', include(admin.site.urls)),
+    
 )
